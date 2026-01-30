6befaff81b8d05cf8ded9ddbccf8e8ae5d4f51be
zhuldyzyelbayeva-spec <zhuldyzyelbayeva@gmail.com>
2026-01-30 16:50:31 +0500

    Update CrudRepository.java

--- src/repository/interfaces/CrudRepository.java
+++ src/repository/interfaces/CrudRepository.java
@@ -1,1 +1,13 @@
 
+package repository.interfaces;
+
+import java.util.List;
+
+public interface CrudRepository<T> {
+
+    void save(T entity);
+    T findById(int id);
+    List<T> findAll();
+    void update(T entity);
+    void delete(int id);
+}
