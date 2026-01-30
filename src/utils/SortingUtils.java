bf2e6c3b41bcea1a034db92d7a1dab2ea4fdebc8
zhuldyzyelbayeva-spec <zhuldyzyelbayeva@gmail.com>
2026-01-30 16:52:10 +0500

    Update SortingUtils.java

--- src/utils/SortingUtils.java
+++ src/utils/SortingUtils.java
@@ -1,1 +1,13 @@
+package utils;
+
+import model.User;
+import java.util.List;
+
+public class SortingUtils {
+
+    public static void sortUsersByRating(List<User> users) {
+        users.sort((u1, u2) ->
+                Double.compare(u2.getRating(), u1.getRating()));
+    }
+}
 
